bdef4c70f61884f639a5f7c862ab275e2e5ec676
Thien-dtu <thycaouyenhuynh331@gmail.com>
2021-10-11 21:29:19 +0700

    base app

---
+++ app/src/main/java/com/shin/dicho/dangNhap.java
@@ -0,0 +1,14 @@
+package com.shin.dicho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
